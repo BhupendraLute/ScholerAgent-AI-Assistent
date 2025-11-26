112599a77fa0a47aa960ebf2192fde5d7176cbf6
Bhupendra Lute <bhupendralute1234@gmail.com.com>
2025-11-26 23:26:37 +0530

    Update root_agent description to clarify its role in coordinating sub-agents for studying, research, and note-taking.

--- root_agent/agent.py
+++ root_agent/agent.py
@@ -9,7 +9,7 @@ from root_agent.sub_agents.notes_taker.agent import notes_taker_agent
 root_agent = LlmAgent(
     model="gemini-2.5-flash",
     name="root_agent",
-    description="The ",
+    description="The main agent that coordinates sub-agents to assist users with studying, research, and note-taking.",
     instruction=prompt.ROOT_AGENT_INSTRUCTIONS,
     sub_agents=[researcher_agent, quiz_master_agent, notes_taker_agent],
 )
